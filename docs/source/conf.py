ba41668afac4fc69f0fda94a539f99941daa8dd5
Jens B. Helmers <jens.bloch.helmers@gmail.com>
2020-10-24 09:38:20 +0200

    sphinx rtd tweak

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,24 +35,24 @@ author = 'Jens B. Helmers'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autosummary',
+    #'sphinx.ext.autosummary',
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
 ]
 
-autosummary_generate = True
+#autosummary_generate = True
 # Napoleon settings
 napoleon_google_docstring = False
 napoleon_numpy_docstring = True
 napoleon_include_init_with_doc = True
-napoleon_include_private_with_doc = False
 napoleon_include_special_with_doc = True
-napoleon_use_admonition_for_examples = False
-napoleon_use_admonition_for_notes = False
-napoleon_use_admonition_for_references = False
-napoleon_use_ivar = False
-napoleon_use_param = False
-napoleon_use_rtype = True
+#napoleon_include_private_with_doc = False
+#napoleon_use_admonition_for_examples = False
+#napoleon_use_admonition_for_notes = False
+#napoleon_use_admonition_for_references = False
+#napoleon_use_ivar = False
+#napoleon_use_param = False
+#napoleon_use_rtype = True
 
 autodoc_member_order = 'bysource'
 
